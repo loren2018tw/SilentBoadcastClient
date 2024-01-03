65d863e4635713852ea19e2bff92cd950cef9d01
Loren Li <loren.tw@gmail.com>
2024-01-03 10:19:46 +0800

    lnk 加入工作目錄

--- public/auto_startup.bat
+++ public/auto_startup.bat
@@ -4,20 +4,22 @@ setlocal enabledelayedexpansion
 REM 設定捷徑名稱和目標路徑
 set "ShortcutName=CKES無聲廣播 客戶端"
 set "TargetPath=C:\SilentBoadcastClient\CKES 無聲廣播系統.exe"
+set "StartInPath=C:\SilentBoadcastClient"  REM 將此路徑更改為您的工作目錄
 
-REM 確保啟動資料夾存在
+REM 建立捷徑在開始資料夾
 if not exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" (
     mkdir "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 )
 
-REM 建立捷徑
-set "ShortcutPath=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\%ShortcutName%.lnk"
+REM 建立捷徑在開始資料夾
+set "ShortcutPath=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\%ShortcutName%.lnk"
 set "VBScript=%TEMP%\CreateShortcut.vbs"
 
 REM 使用 VBScript 建立捷徑
 echo Set WshShell = WScript.CreateObject("WScript.Shell") > "%VBScript%"
 echo Set Shortcut = WshShell.CreateShortcut("%ShortcutPath%") >> "%VBScript%"
 echo Shortcut.TargetPath = "%TargetPath%" >> "%VBScript%"
+echo Shortcut.WorkingDirectory = "%StartInPath%" >> "%VBScript%"
 echo Shortcut.Save >> "%VBScript%"
 
 REM 執行 VBScript 以建立捷徑
